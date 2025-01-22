a3fd82b71aec36d1adc2bbdc772b37c762a9a9fe
Richard King <richard@analytics.com.au>
2025-01-22 21:19:03 +1100

    set base folder - most important and not done by default

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "./",
 })
+
+// base must be set to have assets in a folder under index.html !!!!
